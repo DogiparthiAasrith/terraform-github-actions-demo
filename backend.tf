2d49773af8d1a1bb15c682b5b7f99126fa60497a
DogiparthiAasrith <141314342+DogiparthiAasrith@users.noreply.github.com>
2025-12-19 13:12:16 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-state-demo-12345"
+    bucket = "terraform-tfstate-aasrith-2025-usw2"
     key    = "ec2/terraform.tfstate"
     region = "us-east-1"   # ✅ MUST MATCH BUCKET REGION
   }
