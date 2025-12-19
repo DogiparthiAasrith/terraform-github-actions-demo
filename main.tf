7abdc4c4e0175979bf855b5547350ba46ea98234
DogiparthiAasrith <141314342+DogiparthiAasrith@users.noreply.github.com>
2025-12-19 13:25:04 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,89 @@
+# ---------------------------
+# EC2 Instance
+# ---------------------------
 resource "aws_instance" "demo_ec2" {
-  ami           = "ami-0c02fb55956c7d316" # Amazon Linux 2 (us-east-1)
+  ami           = "ami-0c02fb55956c7d316"
   instance_type = var.instance_type
 
   tags = {
     Name = "GitHub-Actions-EC2"
   }
+}
+
+# ---------------------------
+# IAM Role for Lambda
+# ---------------------------
+resource "aws_iam_role" "lambda_role" {
+  name = "lambda_exec_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_basic_policy" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+# ---------------------------
+# Lambda Function
+# ---------------------------
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  source_file = "lambda.py"
+  output_path = "lambda.zip"
+}
+
+resource "aws_lambda_function" "demo_lambda" {
+  function_name = var.lambda_function_name
+  role          = aws_iam_role.lambda_role.arn
+  handler       = "lambda.lambda_handler"
+  runtime       = "python3.9"
+  filename      = data.archive_file.lambda_zip.output_path
+}
+
+# ---------------------------
+# API Gateway (HTTP API)
+# ---------------------------
+resource "aws_apigatewayv2_api" "http_api" {
+  name          = "demo-http-api"
+  protocol_type = "HTTP"
+}
+
+resource "aws_apigatewayv2_integration" "lambda_integration" {
+  api_id             = aws_apigatewayv2_api.http_api.id
+  integration_type   = "AWS_PROXY"
+  integration_uri    = aws_lambda_function.demo_lambda.invoke_arn
+  integration_method = "POST"
+}
+
+resource "aws_apigatewayv2_route" "default_route" {
+  api_id    = aws_apigatewayv2_api.http_api.id
+  route_key = "GET /"
+  target    = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
+}
+
+resource "aws_apigatewayv2_stage" "default_stage" {
+  api_id      = aws_apigatewayv2_api.http_api.id
+  name        = "$default"
+  auto_deploy = true
+}
+
+# ---------------------------
+# Permission: API Gateway → Lambda
+# ---------------------------
+resource "aws_lambda_permission" "api_gateway_permission" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.demo_lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.http_api.execution_arn}/*/*"
 }
