310afc62cd473f1ee6dcb9cdeb7a1baabd906dec
DogiparthiAasrith <141314342+DogiparthiAasrith@users.noreply.github.com>
2025-12-19 15:38:49 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -9,3 +9,11 @@ output "lambda_function_name" {
 output "api_gateway_url" {
   value = aws_apigatewayv2_api.http_api.api_endpoint
 }
+
+output "native_lock_test_bucket_name" {
+  value = aws_s3_bucket.native_lock_test_bucket.bucket
+}
+
+output "native_lock_test_iam_user" {
+  value = aws_iam_user.native_lock_test_user.name
+}
